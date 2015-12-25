c93ef1bc1789d30fd73a2a8a059426ef26629d61
commology <commology@gmail.com>
2015-12-25 04:45:03 +0000

    refined application framework

--- e2u.sh
+++ e2u.sh
@@ -1,6 +1,14 @@
 #!/bin/env bash
 
 APP_ROOT_DIR=`dirname $0`
+cd $APP_ROOT_DIR
 
-erl -pa $APP_ROOT_DIR/ebin deps/* deps/*/ebin
+ERL_LIBS="."
+DEPS=`ls deps`
+for dep in $DEPS
+do
+    ERL_LIBS="${ERL_LIBS}:./deps/${dep}"
+done
+
+erl -env ERL_LIBS $ERL_LIBS
 
